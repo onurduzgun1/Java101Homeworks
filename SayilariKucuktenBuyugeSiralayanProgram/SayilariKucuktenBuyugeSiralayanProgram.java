02b06c553f0d226c94a763f181babf091d293ef8
Onur Duzgun <102528280+onurduzgun1@users.noreply.github.com>
2022-06-27 11:48:12 +0300

    Add files via upload

---
+++ SayilariKucuktenBuyugeSiralayanProgram/SayilariKucuktenBuyugeSiralayanProgram.java
@@ -0,0 +1,37 @@
+package Java101Odevler.SayilariKucuktenBuyugeSiralayanProgram;
+import java.util.Scanner;
+public class SayilariKucuktenBuyugeSiralayanProgram {
+    public static void main(String[] args) {
+        int a, b, c ;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the first number: ");
+        a = input.nextInt();
+        System.out.print("Enter the second number: ");
+        b = input.nextInt();
+        System.out.print("Enter the third number: ");
+        c = input.nextInt();
+
+        if(a < b && a < c){
+            if(b < c){
+                System.out.println(a + " < " + b + " < " + c);
+            }else {
+                System.out.println(a + " < " + c + " < " + b);
+            }
+        }else if(b < a && b < c){
+            if(a < c){
+                System.out.println(b + " < " + a + " < " + c);
+            }else{
+                System.out.println(b + " < " + c + " < " + a);
+            }
+        }else if(c < a && c < b){
+            if(a < b){
+                System.out.println(c + " < " + a + " < " + b);
+            }else {
+                System.out.println(c + " < " + b + " < " + a);
+            }
+        }else{
+            System.out.println(a + " = " + b + " = " + c);
+        }
+    }
+}
+
