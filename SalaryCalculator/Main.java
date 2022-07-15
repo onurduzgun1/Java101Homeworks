5465eda65abbe94b50519825b69f0f0a6ad0c406
Onur Duzgun <102528280+onurduzgun1@users.noreply.github.com>
2022-07-15 15:30:00 +0300

    Add files via upload

---
+++ SalaryCalculator/Main.java
@@ -0,0 +1,8 @@
+package Java101Odevler.SalaryCalculator;
+
+public class Main {
+    public static void main(String[] args) {
+        Employee e1 = new Employee("Onur Duzgun", 2000, 45, 1985);
+        e1.printEmployee();
+    }
+}
