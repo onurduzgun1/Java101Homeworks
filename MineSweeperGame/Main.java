5df05aba025691d66a672664ce6dacdcba41732e
Onur Duzgun <102528280+onurduzgun1@users.noreply.github.com>
2022-07-16 14:19:50 +0300

    Add files via upload

---
+++ MineSweeperGame/Main.java
@@ -0,0 +1,15 @@
+package Java101Odevler.MineSweeperGame;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Give a number for Line Number: ");
+        int line = input.nextInt();
+        System.out.print("Give a number for Column Number: ");
+        int column = input.nextInt();
+        MineSweeper MineGame = new MineSweeper(line, column);
+        MineGame.RUN();
+    }
+}
